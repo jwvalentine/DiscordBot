d46a5812833aa8b9b51bef9654f1c354fba4f35d
Joe <jwvalentine@outlook.com>
2019-02-19 22:16:14 -0500

    Initial commit
    First Commit of Discord Bot in .NET Core

---
+++ DiscordBot/Core/Commands/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Core.Commands
+{
+    public class HelloWorld : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello"), Alias("hello", "world"), Summary("Hello World Command")]
+        public async Task helloWorld()
+        {
+            await Context.Channel.SendMessageAsync("Hello World!");
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Reflection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+
+namespace DiscordBot
+{
+    class Program
+    {
+        private CommandService Commands;
+        private DiscordSocketClient Client;
+
+        static void Main(string[] args)
+        => new Program().Start()
+            .GetAwaiter()
+            .GetResult();
+
+
+        private async Task Start()
+        {
+
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+
+            // create an empty string to fill from file info
+            string Token = string.Empty;
+
+            // use file stream to fill the token informaiton so that it is not exposed, add file to Git ignore
+            using (var Stream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.0", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(Stream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Bulletbot - Test!", "https://www.twitch.tv/KobeKO", StreamType.NotStreaming);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
+        {
+            var Message = MessageParam as SocketUserMessage;
+            var context = new SocketCommandContext(Client, Message);
+
+            if (context.Message == null || context.Message.Content == string.Empty) return;
+            if (context.User.IsBot) return;
+
+            int ArgPos = 0;
+
+            if (!(Message.HasStringPrefix("!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(context, ArgPos);
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} - Something went wrong with executing a command. Text: {context.Message.Content} | Error: {Result.ErrorReason}");
+            }
+        }
+
+    }
+
+}
